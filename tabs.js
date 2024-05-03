ce468e6a3e1d032d25685dd9d81513d3111c4862
Mayconss4 <mayconsousa91@outlook.com>
2024-05-03 14:36:47 +0100

    Projeto tabs finalizado

--- tabs.js
+++ tabs.js
@@ -2,25 +2,12 @@ $(document).ready(function () {
 });
 
 $('.primary-button').click(function() {
-    $('.primary-button').removeClass('activated');
-    $(this).addClass('activated');
-
     let contentID = $(this).data('content');
     let contentE1 = $('#' + contentID);
 
     $('.body-screen').removeClass('activated');
     contentE1.addClass('activated');
 });
-
-// function clickMenu(){
-//     if (dropdown.style.display=='flex'){
-//         dropdown.style.display='none';
-//         bodies.style.top='0px';
-//     } else {
-//         dropdown.style.display='flex';
-//         bodies.style.top='-700px';
-//     }
-// }
 
 $('#tabs-burguer').click(function(){
     if ($('#tabs-dropdown').css('display') == 'flex'){
